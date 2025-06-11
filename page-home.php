c6a21e2d2db9ff6471de628cc1c7a7a0beb2fc48
Mako Kamiya <mako4kamiya@gmail.com>
2025-06-11 14:40:40 +0900

    ヘッダーとスライダーを含むホームページの初期ファイルを追加

---
+++ page-home.php
@@ -0,0 +1,59 @@
+<?php get_header(); ?>
+
+<main id="Home">
+	<div class="swiper">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide">
+				<img src="../images/sample.png" alt="画像1">
+				<div class="caption">
+					<div class="tags">
+						<span class="tag">#website</span>
+						<span class="tag">#original</span>
+					</div>
+					<p class="title">ポートフォリオサイト</p>
+					<p class="role">Planning / Design /  Coding / WordPress</p>
+				</div>
+				<a href="portfolio.html"></a>
+			</div>
+			<div class="swiper-slide">
+				<img src="../images/sample.png" alt="画像2">
+				<div class="caption">
+					<div class="tags">
+						<span>#website</span>
+						<span>#original</span>
+					</div>
+					<p class="title">ポートフォリオサイト</p>
+					<p class="role">Planning / Design /  Coding / WordPress</p>
+				</div>
+				<a href="portfolio.html"></a>
+			</div>
+			<div class="swiper-slide">
+				<img src="../images/sample.png" alt="画像3">
+				<div class="caption">
+					<div class="tags">
+						<span>#website</span>
+						<span>#original</span>
+					</div>
+					<p class="title">ポートフォリオサイト</p>
+					<p class="role">Planning / Design /  Coding / WordPress</p>
+				</div>
+				<a href="portfolio.html"></a>
+			</div>
+			<div class="swiper-slide">
+				<img src="../images/sample.png" alt="画像4">
+				<div class="caption">
+					<div class="tags">
+						<span>#website</span>
+						<span>#original</span>
+					</div>
+					<p class="title">ポートフォリオサイト</p>
+					<p class="role">Planning / Design /  Coding / WordPress</p>
+				</div>
+				<a href="portfolio.html"></a>
+			</div>
+		</div>
+		<div class="swiper-pagination"></div>
+	</div>
+</main>
+
+<?php get_footer();
