1753dedf6413c9b191d2197e7581fa5db1d3b577
Mako Kamiya <mako4kamiya@gmail.com>
2025-06-21 15:13:15 +0900

    フレームを特定のページだけ表示されるように修正

--- header.php
+++ header.php
@@ -13,21 +13,23 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'makokamiya-wp-theme' ); ?></a>
 
-	<div class="flames is-pc">
-        <div class="frame right"></div>
-        <div class="frame Left"></div>
-		<div class="line top"></div>
-		<div class="line right"></div>
-		<div class="line left"></div>
-        <div class="line bottom"></div>
-    </div>
+	<?php if( is_front_page() || is_singular('works') || is_post_type_archive('works') || is_page('about') ) : ?>
+		<div class="flames is-pc">
+			<div class="frame right"></div>
+			<div class="frame Left"></div>
+			<div class="line top"></div>
+			<div class="line right"></div>
+			<div class="line left"></div>
+			<div class="line bottom"></div>
+		</div>
+	<?php endif; ?>
 	<header id="masthead" class="theme-header site-header">
 		<div class="site-branding">
 			<?php if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a id="HeaderLogo" href="<?php echo esc_url( home_url( '/' ) ); ?>">MAKO KAMIYA</a></h1>
 			<?php else :?>
 				<p class="site-title"><a id="HeaderLogo" href="<?php echo esc_url( home_url( '/' ) ); ?>">MAKO KAMIYA</a></p>
-			<?php endif; ?>				
+			<?php endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
