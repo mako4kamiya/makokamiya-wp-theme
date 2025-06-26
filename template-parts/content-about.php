d2326345b480ca1fe57a6dc8d3c87c1ca5a64cf4
Mako Kamiya <mako4kamiya@gmail.com>
2025-06-26 18:51:42 +0900

    頭痛いから一旦終了

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -5,12 +5,20 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php
+            // 翻訳タイトルを表示
+            $translation_title = get_post_meta( get_the_ID(), 'translation_title', true );
+            if ( $translation_title ) {
+                echo '<span class="translation-title">' . esc_html( $translation_title ) . '</span>';
+            }
+        ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php 
 			$author_id = $admin_user->ID;
 			echo get_avatar($author_id, 96);
+			echo '<h2>' . esc_html(get_the_author_meta('display_name', $author_id)) . '</h2>';
 			the_content();
 		?>
 	</div><!-- .entry-content -->
